9a98497b64b54ab40024f19982cd1d00e55b4c73
Oleksandr <netesa.sasha@gmail.com>
2024-05-19 18:46:42 +0300

    /1

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,22 @@
 console.log("Task-3");
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUsername(newName) {
+    this.username = "Marco";
+  },
+  updatePlayTime(hours) {
+    this.playTime += 20;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
